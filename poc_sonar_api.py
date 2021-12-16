3b719a207f7e76754c8bb66fb3ff80f118b5a370
rooparishik <88215024+rooparishik@users.noreply.github.com>
2021-12-16 11:56:20 -0500

    Update poc_sonar_api.py

--- poc_sonar_api.py
+++ poc_sonar_api.py
@@ -3,6 +3,6 @@ from requests.auth import HTTPBasicAuth
 token = 'SONAR_TOKEN'
 PARAM = {'component': 'mvn-poc', 'metrickeys': 'ncloc,complexity,violations'}
 test_url = 'http://34.211.190.58:9000/api/measures/component'
-test_response = requests.get(test_url,auth=HTTPBasicAuth(username=token, password=""), verify=False,params=PARAM)
+test_response = requests.get(test_url,auth=HTTPBasicAuth(username=admin, password="admin"), verify=False,params=PARAM)
 test_json = test_response.json()
 print(test_json)
